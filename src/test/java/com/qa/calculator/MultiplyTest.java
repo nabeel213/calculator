ebdcb7007f9d6acbee259af361dba408f1be0de2
nabeel <nabeel.94@live.com>
2018-04-19 10:53:21 +0100

    dfg

--- src/test/java/com/qa/calculator/MultiplyTest.java
+++ src/test/java/com/qa/calculator/MultiplyTest.java
@@ -2,14 +2,19 @@ package com.qa.calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MultiplyTest {
 
-	private 
+	private Multiply multi;
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		multi = new Multiply();
+		int expectedValue = 4;
+		int actualValue = multi.multiplyNums(2, 2);
+		Assert.assertEquals(expectedValue, actualValue);
+		
 	}
 
 }
